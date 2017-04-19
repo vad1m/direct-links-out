c37a0dba5db453c56b7b194821502a980a5ba69f
Vadim <vad1m@users.noreply.github.com>
2017-04-19 12:18:32 +0200

    Added forumavia.ru support (#3)

--- direct-links-out.user.js
+++ direct-links-out.user.js
@@ -94,6 +94,8 @@
 //taker
 // @match       *://taker.im/*
 // @match       *://*.taker.im/*
+//forumavia
+// @match       *://*.forumavia.ru/*
 
 // ==/UserScript==
 (function() {
@@ -264,6 +266,8 @@
             anchor = "go/?";
         else if (/wikimapia.org/i.test(loc))
             anchor = 'external_link?url=';
+        else if (/forumavia.ru/i.test(loc))
+            anchor = '/e/?l=';
         else if (/picarto/i.test(loc)){
             anchor = "referrer?go=";
             after = "&ref=";
